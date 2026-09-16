170439b5868bd51bd751553ef1202148ea630105
JOSH HORNETT MAGNO <25-000124@mcstayuman.edu.ph>
2026-09-16 16:07:59 +0800

    MagnoPeta1

---
+++ app/src/test/java/com/example/recivox/quarter2/MagnoPeta1.java
@@ -0,0 +1,23 @@
+package com.example.recivox.quarter2;
+
+import org.junit.Test;
+
+public class MagnoPeta1 {
+    @Test
+    public void thisisMyInfo() {
+
+        //--- 1. THE INPUT (Storing your personal details in variables) -
+        String myName = "Josh Magno";
+        String favGame = "Valorant";
+        String favFood = "Siomai";
+        int Age = 17;
+
+// 2. THE OUTPUT (Printing to the console)
+        System.out.println("----- MY INFORMATION -----");
+        System.out.println("Hello, I am "+myName +" currently, "+Age +" years old.");
+        System.out.println("My favorite game to play is "+favGame +".");
+        System.out.println("To hit my protein intake, "+favFood +" gets me through the day!");
+
+    }
+}
+
