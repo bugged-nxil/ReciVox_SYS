c3c86a4aaeceeedc56a081aaacfd0f8843d68f90
bryanmanlutac13 <bryanmanlutac13@gmail.com>
2026-09-18 10:41:30 +0800

    091826 TRIA JAVA CLASS New code!! woohoo

---
+++ app/src/test/java/com/example/recivox/quarter2/test/quarter2/TriaPeta1.java
@@ -0,0 +1,22 @@
+package com.example.recivox.quarter2.test.quarter2;
+
+import org.junit.Test;
+
+public class TriaPeta1 {
+    @Test
+    public void thisisMyInfo() {
+
+        //--- 1. THE INPUT (Storing your personal details in variables) -
+        String myName = "Jairus Tria";
+        String favGame = "Call of Duty";
+        String favFood = "Fried Chicken";
+        int Age = 17;
+
+// 2. THE OUTPUT (Printing to the console)
+        System.out.println("----- MY INFORMATION -----");
+        System.out.println("Hello, I am "+myName +" currently, "+Age +" years old.");
+        System.out.println("My favorite game to play is "+favGame +".");
+        System.out.println("To play basketball, "+favFood +" gets me through the day!");
+
+    }
+}
